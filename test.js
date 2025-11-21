f7818a23f5dae62dbe4a64f1aec4c76b406c7a0f
Peter Dickx <peter.dickx@gmail.com>
2025-11-21 22:28:47 +0100

    added formatting

--- test.js
+++ test.js
@@ -1,7 +1,33 @@
-let a = 0;
-for (let i = 0; i < 10; i++) {
+let xPositions = [];
+let yPositions = [];
+let colors = [];
+
+function setup() {
+    colorMode(HSB);
+    createCanvas(windowWidth, windowHeight);
+    for (let i = 0; i < 1000; i++) {
+        xPositions[i] = random(0, width);
+        yPositions[i] = random(0, height);
+        colors[i] = random(0, 360);
+    }
+    drawGhost(width / 2, height / 2, 0);
 }
-while
-if
-else 
-    else if
+
+function draw() {
+    background("black");
+    for (let i = 0; i < xPositions.length; i++) {
+        drawGhost(xPositions[i], yPositions[i], colors[i]);
+        xPositions[i] += random(-2, 2);
+        yPositions[i] += random(-2, 2);
+    }
+}
+
+function draw2() {
+    background("black");
+    for (let i = 0; i < xPositions.length; i++) {
+        drawGhost(xPositions[i], yPositions[i], colors[i]);
+        xPositions[i] += random(-2, 2);
+        yPositions[i] += random(-2, 2);
+    }
+}
+
